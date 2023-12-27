51781bc16392bcfd6ddde3d0e5de73921cb191ba
Olga <minaeva9@gmail.com>
2023-12-27 18:16:42 -0500

    js modified

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById("modal");
 const closeModal = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -7,4 +9,31 @@ setTimeout(function () {
 
 closeModal.addEventListener("click", function () {
   modal.style.display = "none";
+});
+
+consentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const consentFormData = new FormData(consentForm);
+  const fullName = consentFormData.get("name");
+
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`;
+
+  setTimeout(function () {
+    document.getElementById("upload-text").textContent = `Making the sale...`;
+  }, 1500);
+
+  setTimeout(function () {
+    document.getElementById("modal-inner").innerHTML = `
+  <h2>Thanks <span class="modal-display-name">${fullName}</span> sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+  `;
+  }, 3000);
 });
